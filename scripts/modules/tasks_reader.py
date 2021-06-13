f2dcc76cd928613498fa6b9c8fe184c4ff144a7e
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2021-06-13 14:15:52 +0700

    Добавил поддержку групп и плавающего времени

--- scripts/modules/tasks_reader.py
+++ scripts/modules/tasks_reader.py
@@ -1,3 +1,5 @@
+import re
+
 def get_tasks_from_file(path, current_date):
 
     def get_new_task(title = ''):
@@ -10,48 +12,79 @@ def get_tasks_from_file(path, current_date):
             'recurrence':   '',            
         }
 
+    def get_new_group(title = ''):
+
+        return {
+            'title': title,
+            'tasks': []
+        }
+
+    def add_group():
+
+        if is_group():
+            groups.append(group)
+
     def add_task():
 
         if is_task():
-            tasks.append(task)            
+            group['tasks'].append(task)            
+
+    def is_group():
+
+        return group['title'] != ''
 
     def is_task():
 
         return task['title'] != ''
 
-    tasks = []
+    groups = []
 
     with open(path, encoding = 'utf-8-sig') as handle:
         
-        lines   = handle.read().splitlines()
-        task    = get_new_task()
-            
+        lines   = handle.read().splitlines()        
+        group   = get_new_group()
+        task    = get_new_task()        
+                    
         for line in lines:
                 
             if line.startswith('-') or line.startswith('+'):
                     
                 add_task()
 
-                parts       = line[1:].strip().split('     ')
-                title_date  = parts[0].strip()
-                title_name  = parts[1].strip()
+                line_without_mark = line[1:].strip()
 
-                title_date_parts = title_date.strip().split(':')
+                time = re.search(r'[0-9]{1,2}:[0-9]{2}', line_without_mark)
 
-                hour    = int(title_date_parts[0])
-                minute  = int(title_date_parts[1])
+                if time != None:
+
+                    title_date  = time.group(0)
+                    title_date_parts = title_date.strip().split(':')
+
+                    hour    = int(title_date_parts[0])
+                    minute  = int(title_date_parts[1])                    
+
+                else:
+
+                    hour    = 0
+                    minute  = 0
 
                 task = get_new_task()
 
-                task['title']       = title_name
+                task['title']       = line_without_mark
                 task['datetime']    = current_date.replace(hour = hour, minute = minute)
                 task['completed']   = line.startswith('+')
-                
+
             elif line.startswith('#'):
 
                 add_task()
 
                 task = get_new_task()
+
+                if group['title'] == '':
+                    group['title'] = line
+                else:
+                    add_group()
+                    group = get_new_group(line)
 
             else:
 
@@ -59,5 +92,6 @@ def get_tasks_from_file(path, current_date):
                     task['notes'].append(line)
 
         add_task()
+        add_group()
     
-    return tasks
+    return groups
