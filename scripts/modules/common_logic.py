da09e2dcaa18f484db1d28d6ad0d4b4bbffc3f10
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2021-08-16 20:18:22 +0700

    Напоминания через Телеграм

--- scripts/modules/common_logic.py
+++ scripts/modules/common_logic.py
@@ -5,12 +5,14 @@ def get_paths(script_dirpath):
 
     tasks_dirpath = os.path.split(script_dirpath)[0]
 
-    parameters_filepath     = os.path.join(tasks_dirpath, 'cache.yaml')
+    cache_filepath          = os.path.join(tasks_dirpath, 'cache.yaml')
+    settings_filepath       = os.path.join(tasks_dirpath, 'tuner.yaml')
     current_tasks_filepath  = os.path.join(tasks_dirpath, 'today.md')
     planned_tasks_filepath  = os.path.join(tasks_dirpath, 'plans.md')    
     
     return {
-        'parameters_filepath':      parameters_filepath,
+        'settings_filepath':        settings_filepath,
+        'cache_filepath':           cache_filepath,
         'tasks_dirpath':            tasks_dirpath,
         'current_tasks_filepath':   current_tasks_filepath,
         'planned_tasks_filepath':   planned_tasks_filepath,
--- scripts/ringer.py
+++ scripts/ringer.py
@@ -1,9 +1,10 @@
 import os
 import re
 import datetime
-import winsound
+import requests
 
 import modules.tasks_reader as tasks_reader
+import modules.yaml_wrapper as yaml_wrapper
 import modules.common_logic as common_logic
 
 def get_notifications():
@@ -55,14 +56,31 @@ def get_notifications():
                notifications.append(group)
             else:
                notifications.append(group[0])
+   
+   notifications = []
 
-   notifications  = []
-   content        = get_content()
+   content = get_content()
   
    find_notifications_before_start(content)
    find_notifications_after_start(content)
 
    return notifications
+
+def send_to_telegram(text: str):
+
+   url = 'https://api.telegram.org/bot'
+   url += settings['telegram_bot_api_token']
+   url += '/sendMessage'
+
+   data = {
+      'chat_id':  settings['telegram_chat_id'],
+      'text':     text
+   }
+
+   result = requests.post(url, data)
+
+   if result.status_code != 200:
+      raise Exception('Unable to send a message via Telegram!')
 
 script_dirpath    = os.path.abspath(os.path.dirname(__file__))
 current_date      = common_logic.get_current_date()
@@ -70,9 +88,10 @@ current_date      = common_logic.get_current_date()
 paths = common_logic.get_paths(script_dirpath)
 tasks = tasks_reader.get_tasks_from_file(paths['current_tasks_filepath'], current_date)
 
-now = datetime.datetime.now()
+settings = yaml_wrapper.get_data_from_file(paths['settings_filepath'])
 
-ring = False
+now      = datetime.datetime.now()
+titles   = []
 
 for group in tasks:
 
@@ -84,8 +103,7 @@ for group in tasks:
       notifications = get_notifications()
 
       if len(notifications) > 0:
-         ring = True
-         break
+         titles.append(task['title'])
 
-if ring:
-   winsound.Beep(300, 1200)
+titles = '\n'.join(titles)
+send_to_telegram(titles)
