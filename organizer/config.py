987b3e5091f24ceebc017eb97d4c7a06895b8e81
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2022-06-21 20:47:32 +0700

    Changed import declaration

--- organizer/config.py
+++ organizer/config.py
@@ -1,18 +1,18 @@
-import os.path as path
+from os.path import dirname, join
 
 
 def get_tasks_file_path() -> str:
-    directory = path.dirname(path.dirname(__file__))
+    directory = dirname(dirname(__file__))
     file_name = "tasks.md"
 
-    return path.join(directory, file_name)
+    return join(directory, file_name)
 
 
 def get_plans_file_path() -> str:
-    directory = path.dirname(path.dirname(__file__))
+    directory = dirname(dirname(__file__))
     file_name = "plans.md"
 
-    return path.join(directory, file_name)
+    return join(directory, file_name)
 
 
 def is_date(line: str) -> bool:
