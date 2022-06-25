8edd9d7864011064f5799fa9cdcea6c93820756f
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2022-06-25 16:40:45 +0700

    Logic of checking for incomplete dates

--- organizer.py
+++ organizer.py
@@ -3,8 +3,7 @@ import datetime
 from consolemenu import *
 from consolemenu.items import *
 
-from tasks_file_format import *
-from tasks_file_parser import *
+from tasks_file import *
 
 import os.path as path
 
@@ -27,6 +26,25 @@ def tasks_in_progress(file_items: list) -> list:
     return []
 
 
+def get_incomplete_days(file_items: list) -> list:
+    current_date = datetime.date.today()
+    incomplete_days = []
+
+    for file_item in file_items:
+
+        if type(file_item) == Date and file_item.date <= current_date:
+
+            scheduled_tasks = file_item.get_scheduled_tasks()
+
+            if len(scheduled_tasks) > 0:
+                incomplete_day = Date(file_item.lines[0])
+                incomplete_day.items = scheduled_tasks
+
+                incomplete_days.append(incomplete_day)
+
+    return incomplete_days
+
+
 def update_tasks() -> None:
     """
     Creates tasks for the current day (and days before, in case the script wasn't called for them previously),
@@ -38,9 +56,20 @@ def update_tasks() -> None:
 
     tasks_file_path = get_tasks_file_path()
 
-    file_items = TasksFileParser(tasks_file_path).parse()
+    file_items = Parser(tasks_file_path).parse()
 
-    current_date = datetime.date.today()
+    incomplete_days = get_incomplete_days(file_items)
+
+    if len(incomplete_days) > 0:
+
+        print("Unable to perform, since there is at least one incomplete task:\n")
+
+        for incomplete_day in incomplete_days:
+            print(incomplete_day)
+            print()
+
+        print("You have to rearrange it or mark it as complete or cancelled.")
+
 
 def trigger_menu_item_update_tasks(prompt_utils: PromptUtils):
 
