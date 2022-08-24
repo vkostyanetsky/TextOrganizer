94b69630a6c17112dcf67c11fba4134fdb513f3d
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2022-08-24 23:55:38 +0700

    Settings for package

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from organizer.version import __version__
+
+
+setup(
+    name="organizer",
+    version=__version__,
+    description="A simple CLI tool to deal with tasks",
+    long_description=open("README.md", encoding="utf-8-sig").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/vkostyanetsky/TextOrganizer",
+    license="MIT",
+    python_requires=">=3.7",
+    packages=["organizer"],
+    install_requires=[
+        "PyYAML~=6.0",
+        "keyboard~=0.13.5",
+        "vkostyanetsky.cliutils~=0.2.0",
+    ],
+    entry_points={"console_scripts": ["organizer=organizer.app:main"]},
+    author="Vlad Kostyanetsky",
+    author_email="vlad@kostyanetsky.me",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Utilities",
+    ],
+    keywords="organizer task",
+)
